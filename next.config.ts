f95f5fa90fe4fdead0c815179f9aa85b472f88c4
Ifeanyi Lucky <ifeanyilucky360@gmail.com>
2026-02-22 09:12:54 +0100

    feat: configure Next.js to allow images from Cloudinary, Google, and Unsplash domains.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  experimental: {
+    // any experimental features if needed
+  },
 };
 
 export default nextConfig;
