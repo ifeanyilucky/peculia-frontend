2b2b8dea503fda402c43cd5ff747f83fbfdc9967
Ifeanyi Lucky <ifeanyilucky360@gmail.com>
2026-02-23 22:13:36 +0100

    config: Disable system theme preference in ThemeProvider by setting `enableSystem` to `false`.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,7 +92,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
-          enableSystem
+          enableSystem={false}
           disableTransitionOnChange
         >
           <QueryProvider>
